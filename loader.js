112bf20f2a247896cffdf7de16bbc086e829a0a1
iki1uc <entertrainer@web.de>
2026-07-24 05:51:17 +0200

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,32 +1,38 @@
 export async function loadUniversal() {
 
-  // Master-Kern laden
+  // Master-Kern
   const ALL = await import("../ALL/all.js");
 
-  // RUN8 / VECTOR / kanal laden
+  // RUN8 / VECTOR / kanal
   const RUN8 = await import("../RUN8.bind.js");
   const VECTOR = await import("../VECTOR.pipe.js");
   const KANAL = await import("../kanal.js");
 
-  // Matrix laden
+  // Validierung
+  const ARG = await import("../ALL/arg.js");
+
+  // Matrix
   const M81 = await import("../81/root.json", { assert: { type: "json" } });
   const MRAW = await import("../9hoch9/root.json", { assert: { type: "json" } });
   const MFULL = await import("../81_81_1/root.json", { assert: { type: "json" } });
 
-  // 4u KI laden
+  // VECTOR-Häppchen
+  const VCHUNK = await import("../VECTOR/chunks.js");
+
+  // 4u KI (externe Datei)
   const FOURU = await import("file:///D:/DE/4u.js");
 
-  // Symbiose herstellen
-  const symbiose = {
+  // Symbiose
+  window.AnPI = {
     ALL,
     RUN8,
     VECTOR,
     KANAL,
+    ARG,
     M81,
     MRAW,
     MFULL,
+    VCHUNK,
     FOURU
   };
-
-  window.AnPI = symbiose;
 }
